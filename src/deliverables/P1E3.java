4b6adb65072a59498773eecc45a9c14b535c66e0
IMora04 <ignmorper1@alum.us.es>
2023-10-18 23:19:29 +0200

    E3 update

--- src/deliverables/P1E3.java
+++ src/deliverables/P1E3.java
@@ -13,11 +13,13 @@ public class P1E3 {
 
 		List<String> res = new ArrayList<String>();
 		try {
-			Iterator<String> iterator1 = Files.lines(Path.of(fileA)).iterator();
+			Iterator<String> iterator1 = Files.lines(Path.of(fileA)).iterator();	//Initializing iterators and variables used
 			Iterator<String> iterator2 = Files.lines(Path.of(fileB)).iterator();
+
 			int counterTo2 = 0;
 			boolean isFirst = true;
-			while (iterator1.hasNext() && iterator2.hasNext()) {
+			
+			while (iterator1.hasNext() && iterator2.hasNext()) {	//If both of them have next, iterate
 				if (isFirst) {
 					res.add(iterator1.next());
 				} else {
@@ -30,15 +32,17 @@ public class P1E3 {
 					counterTo2 = 1;
 				}
 			}
-			if (!iterator1.hasNext()) {
-				while (iterator2.hasNext()) {
+			
+			if (!iterator1.hasNext()) {		//If the iteration stopped, then
+				while (iterator2.hasNext()) {	//Iterator 1 doesn't have next
 					res.add(iterator2.next());
 				}
-			} else if (!iterator2.hasNext()) {
+			} else if (!iterator2.hasNext()) {	//Iterator 2 doesn't have next
 				while (iterator1.hasNext()) {
 					res.add(iterator1.next());
 				}
-			}
+			}									//Both don't have next
+			
 			return res;
 
 		} catch (IOException e) {
@@ -47,7 +51,4 @@ public class P1E3 {
 		return res;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(ex3_iterative("files/PI1Ej3DatosEntrada1A.txt", "files/PI1Ej3DatosEntrada1B.txt"));
-	}
 }
