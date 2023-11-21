3569220340dab5881aaaf997dc407c720da980c7
IMora04 <ignaciomorap04@gmail.com>
2023-11-21 21:01:22 +0100

    PI2 Ex3 Correction and tests

--- src/exercisesP2/Exercise3.java
+++ src/exercisesP2/Exercise3.java
@@ -1,5 +1,9 @@
 package exercisesP2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import us.lsi.common.Pair;
 import us.lsi.tiposrecursivos.BinaryTree;
 import us.lsi.tiposrecursivos.BinaryTree.BEmpty;
@@ -12,80 +16,57 @@ import us.lsi.tiposrecursivos.Tree.TNary;
 
 public class Exercise3 {
 	
-	public static Boolean isBalanced(BinaryTree<Character> tree) {
-		return switch(tree) {
-			case BEmpty<Character> t -> {
-				yield true;
-			}
-			case BLeaf<Character> t -> {
-				yield true;
-			}
-			case BTree<Character> t -> {
-				yield (t.left().height() - t.right().height() <= 1 &&
-						t.left().height() - t.right().height() >= -1 &&
-						isBalanced(t.left()) &&
-						isBalanced(t.right()));
-			}
-		};
-	}
 	
-	public static Boolean isBalanced2(BinaryTree<Character> tree) {
+	public static Boolean isBalanced(BinaryTree<Character> tree) {
 		return isBalancedAux(tree, 0).first();
 	}
 	
 	public static Pair<Boolean,Integer> isBalancedAux(BinaryTree<Character> tree, Integer height) {
 		return switch(tree) {
-		case BEmpty<Character> t-> {
-			yield Pair.of(true,height);
-		}
-		case BLeaf<Character> t-> {
-			yield Pair.of(true,height);
-		}
-		case BTree<Character> t-> {
-			height +=1;
-			Pair<Boolean, Integer> tL = isBalancedAux(t.left(), height);
-			Pair<Boolean, Integer> tR = isBalancedAux(t.right(), height);
-			yield Pair.of(
-					tL.first() && tR.first() && (Math.abs(tL.second() - tR.second()) <= 1), 
-					Math.max(tL.second(), tR.second()));
-		}
+			case BEmpty<Character> t-> {
+				yield Pair.of(true,height);
+			}
+			case BLeaf<Character> t-> {
+				yield Pair.of(true,height);
+			}
+			case BTree<Character> t-> {
+				height +=1;
+				Pair<Boolean, Integer> tL = isBalancedAux(t.left(), height);
+				Pair<Boolean, Integer> tR = isBalancedAux(t.right(), height);
+				yield Pair.of(
+						tL.first() && tR.first() && (Math.abs(tL.second() - tR.second()) <= 1), 
+						Math.max(tL.second(), tR.second()));
+			}
 		};
 	}	
+	
 	public static Boolean isBalanced(Tree<Character> tree) {
+		return isBalancedAux(tree, 0).first();
+	}
+	
+	public static Pair<Boolean, Integer> isBalancedAux(Tree<Character> tree, Integer height) {
 		return switch(tree) {
 			case TEmpty<Character> t -> {
-				yield true;
+				yield Pair.of(true, height);
 			}
 			case TLeaf<Character> t -> {
-				yield true;
+				yield Pair.of(true, height);
 			}
 			case TNary<Character> t -> {
-				for(int i = 0; i < t.children().size(); i++) {
-					for(int j = i; j < t.children().size(); j++) {
-						if(!(t.children().get(i).height() - t.children().get(j).height() <= 1 &&
-								t.children().get(i).height() - t.children().get(j).height() >= -1 &&
-								isBalanced(t.children().get(j)))) {
-							yield false;
-						}
+				height += 1;
+				List<Integer> heights = new ArrayList<>();
+				
+				for(Tree<Character> c:t.children()) {
+					Pair<Boolean, Integer> iB = isBalancedAux(c, height);
+					if(!iB.first()) {
+						yield Pair.of(false, 0);
 					}
+					heights.add(iB.second());
 				}
-				yield true;
+				Collections.sort(heights, Collections.reverseOrder());
+				yield Pair.of(Math.abs(heights.get(0) - heights.get(heights.size() -1)) <= 1, heights.get(0));
 			}
 		};
 	}
 	
-	public static void main(String[] args) {
-		BinaryTree<Character> b1 = BinaryTree.parse("A(B(_,G(H,I)),C(D,E(F,_)))", s -> s.charAt(0));
-		BinaryTree<Character> b2 = BinaryTree.parse("A(B(/_,G(H,I)),C(D,E(F,/_)))", s -> s.charAt(0));
-		System.out.println(b1);
-		System.out.println(isBalanced2(b1));
-		System.out.println(isBalanced(b1));
-		System.out.println(b2);
-		System.out.println(isBalanced2(b2));
-		System.out.println(isBalanced(b2));
-
-	//	BinaryTree<Character> b2 = BinaryTree.parse("A(B(D,E),C(F,G(H,I)))", s -> s.charAt(0));
-	//	System.out.println(isBalanced(b2));
-	}
-
 }
--- src/exercisesTestsP2/Exercise3Test.java
+++ src/exercisesTestsP2/Exercise3Test.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import us.lsi.tiposrecursivos.BinaryTree;
+import us.lsi.tiposrecursivos.Tree;
 
 public class Exercise3Test {
 	
@@ -18,33 +19,27 @@ public class Exercise3Test {
 			e.printStackTrace();
 		}
 		for(String s:values) {
-			System.out.println("----- TEST FOR TREE: " + s + " -----");
+			System.out.println("----- TEST FOR BINARY TREE: " + s + " -----");
 			System.out.println(exercisesP2.Exercise3.isBalanced(BinaryTree.parse(s, a -> a.charAt(0))));
 		}
 	}
 
-	
-	public static void testBin2() {
+	public static void testNAry() {
 		List<String> values = new ArrayList<String>();
 		try {
-			values = Files.lines(Path.of("files/PI2Ej3DatosEntradaBinary.txt")).toList();
+			values = Files.lines(Path.of("files/PI2Ej3DatosEntradaNary.txt")).toList();
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 		for(String s:values) {
 			System.out.println("----- TEST FOR TREE: " + s + " -----");
-			System.out.println(exercisesP2.Exercise3.isBalanced2(BinaryTree.parse(s, a -> a.charAt(0))));
+			System.out.println(exercisesP2.Exercise3.isBalanced(Tree.parse(s, a -> a.charAt(0))));
 		}
-	}
-	
-	public static void testNary() {
-		
 	}
 	
 	public static void main(String[] args) {
 		testBin();
-		testBin2();
-		//testBin2();
+		testNAry();
 	}
 
 }
