3842d4760084ed6f1aefb5c8b0e084442215a775
IMora04 <ignaciomorap04@gmail.com>
2023-11-22 17:35:01 +0100

    Module fix

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module ADDA_LABS {
+	exports auxTypesP1;
+	exports exercisesTestsP1;
+	exports exercisesP2;
+	exports exercisesP1;
+	exports exercisesTestsP2;
+
+	requires commons.math3;
+	requires partecomun;
+}
